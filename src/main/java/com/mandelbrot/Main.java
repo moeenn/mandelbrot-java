0ab3613b415a99841ad36ee8dd131ff9de73f998
moeenn <moeen.v8@gmail.com>
2026-06-07 07:20:29 +0500

    updated output

--- src/main/java/com/mandelbrot/Main.java
+++ src/main/java/com/mandelbrot/Main.java
@@ -7,8 +7,8 @@ import java.io.File;
 
 public class Main {
     static void run() throws Exception {
-        int height = 5_000;
-        int width = 5_000;
+        int height = 2_000;
+        int width = 2_000;
         var filename = String.format("plot_%dx%d.png", width, height);
 
         var image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
@@ -19,13 +19,18 @@ public class Main {
 
         graphics.dispose();
         ImageIO.write(image, "png", new File(filename));
+        System.out.printf("%10s | image written to disk.\n", "info");
     }
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         try {
             Main.run();
         } catch (Exception ex) {
             System.err.printf("error: %s.\n", ex.getMessage());
         }
+
+        long elapsed = (System.currentTimeMillis() - start);
+        System.out.printf("%10s | %d seconds\n", "elapsed", elapsed);
     }
 }
